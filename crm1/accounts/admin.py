89474bd57fac7c6119c387a375f1867c1579e8bd
Jhon Esteban Luengas Machado <jeluengasm@unal.edu.co>
2021-08-23 13:32:27 -0500

    Lesson 5

--- crm1/accounts/admin.py
+++ crm1/accounts/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import * # We must import this table created and migrated from "models.py"
+
+admin.site.register(Customer)
+admin.site.register(Product)
+admin.site.register(Order)
