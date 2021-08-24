1f7e87e22ece0e69e3388bda0e637d4b347d12d1
Jhon Esteban Luengas Machado <jeluengasm@unal.edu.co>
2021-08-24 00:50:22 -0500

    Lesson 9.5

--- crm1/accounts/urls.py
+++ crm1/accounts/urls.py
@@ -4,7 +4,7 @@ from django.urls import path
 from . import views # Import the "views.py" file
 
 urlpatterns = [
-    path('', views.home), # Write as a signal, no as a function or method!!!
-    path('products/',views.products),
-    path('customer/<str:pk_test>',views.customer)
+    path('', views.home, name="home"), # Write as a signal, no as a function or method!!!
+    path('products/',views.products, name="products"),
+    path('customer/<str:pk_test>',views.customer, name="customer")
 ]
