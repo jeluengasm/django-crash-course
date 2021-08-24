6dcf3af3c4efde9348938028603494dce38d83d6
Jhon Esteban Luengas Machado <jeluengasm@unal.edu.co>
2021-08-24 00:08:05 -0500

    Lesson 8

--- crm1/accounts/models.py
+++ crm1/accounts/models.py
@@ -29,7 +29,6 @@ class Product(models.Model):
     date_created = models.DateTimeField(auto_now_add=True, null=True)
     tags = models.ManyToManyField(Tag)
     
-    
     def __str__(self):
         return self.name
     
--- crm1/accounts/views.py
+++ crm1/accounts/views.py
@@ -1,15 +1,31 @@
 from django.shortcuts import render # We must use this function to render
 from django.http import HttpResponse # Import library
+from .models import *
 
 # Create your views here.
 
 ## Create functions in the view's file
 
 def home(request):
-    return render(request, 'accounts/dashboard.html') # First render
+    orders = Order.objects.all()
+    customers = Customer.objects.all()
+    
+    total_customers = customers.count()
+    total_orders = orders.count()
+    
+    delivered = orders.filter(status='Delivered').count()
+    pending = orders.filter(status='Pending').count()
+    
+    context = {'orders': orders, 'customers': customers,
+               'total_customers': total_customers, 'total_orders': total_orders,
+               'delivered': delivered, 'pending': pending}
+    
+    return render(request, 'accounts/dashboard.html', context) # First render
 
 def products(request):
-    return render(request, 'accounts/products.html') # First render
+    products = Product.objects.all()
+    
+    return render(request, 'accounts/products.html', {'products': products}) # First render
 
 def customer(request):
     return render(request, 'accounts/customers.html') # First render
